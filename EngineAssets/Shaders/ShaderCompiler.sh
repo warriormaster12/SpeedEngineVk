262eb2ee3f15d1d582422660996a837c90586094
warriormaster12 <streng.alexander@gmail.com>
2020-05-09 15:29:42 +0300

    Updated shader compiler script

--- EngineAssets/Shaders/ShaderCompiler.sh
+++ EngineAssets/Shaders/ShaderCompiler.sh
@@ -1,2 +1,2 @@
-/home/alexander/ProgrammingProjects/C++/Vulkan/lib/Vulkan/x86_64/bin/glslc Shader.vert -o vert.spv
-/home/alexander/ProgrammingProjects/C++/Vulkan/lib/Vulkan/x86_64/bin/glslc Shader.frag -o frag.spv
+../../lib/Vulkan/x86_64/bin/glslc Shader.vert -o vert.spv
+../../lib/Vulkan/x86_64/bin/glslc Shader.frag -o frag.spv
