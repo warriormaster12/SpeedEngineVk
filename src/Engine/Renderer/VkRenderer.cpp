0987e21ed037b1bd99b1126c5aa249e2b39c2dfa
warriormaster12 <streng.alexander@gmail.com>
2020-10-10 21:38:09 +0300

    Removed rendererconst.cpp file

--- src/Engine/Renderer/VkRenderer.cpp
+++ src/Engine/Renderer/VkRenderer.cpp
@@ -3,6 +3,47 @@
 
 namespace VkRenderer
 {
+    Renderer::Renderer()
+    {
+
+        //GraphicsPipeline
+        gpipeline_ref.setup_ref = &setup_ref;
+        lightpipeline_ref.setup_ref = &setup_ref;
+
+        //RenderPass
+        renderpass_ref.Dbuffer_ref = &Dbuffer_ref;
+
+        //UniformBuffer
+        uniformBuffer_ref.camera_object.win_ref = &win_ref;
+        
+        //BufferCreation
+        buffer_ref.setup_ref = &setup_ref;
+        buffer_ref.memory_ref = &memory_ref;
+        
+        
+
+        //CommandBuffer
+        Cbuffer_ref.setup_ref = &setup_ref;
+        Cbuffer_ref.gpipeline_ref = &gpipeline_ref;
+        Cbuffer_ref.lightpipeline_ref = &lightpipeline_ref;
+        Cbuffer_ref.renderpass_ref = &renderpass_ref;
+
+        //FrameBuffer
+        Fbuffer_ref.setup_ref = &setup_ref;
+        Fbuffer_ref.swap_ref = &swap_ref;
+        Fbuffer_ref.renderpass_ref = &renderpass_ref;
+        Fbuffer_ref.Dbuffer_ref = &Dbuffer_ref;
+
+        
+        //DepthBuffer
+        Dbuffer_ref.setup_ref = &setup_ref;
+        Dbuffer_ref.image_m_ref = &image_m_ref;
+
+        //imageManager
+        image_m_ref.setup_ref = &setup_ref;
+        image_m_ref.buffer_ref = &buffer_ref;
+        image_m_ref.memory_ref = &memory_ref;
+    }
     
     void Renderer::InitVulkan()
     {
